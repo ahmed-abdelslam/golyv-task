712c5f2c2a196c4b31ba30f8512afaff6e4e53b2
Ahmed Abdelslam <ahmed.nabolsy6688@gmail.com>
2023-09-13 21:18:02 +0300

    add user relationship in Seat Model

--- app/Models/Seat.php
+++ app/Models/Seat.php
@@ -5,15 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Bus;
+use App\Models\User;
 
 class Seat extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['is_booked', 'bus_id'];
+    protected $fillable = ['is_booked', 'bus_id', 'user_id'];
 
     public function bus()
     {
         return $this->belongsTo(Bus::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }

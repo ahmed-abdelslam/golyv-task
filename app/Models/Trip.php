86545116fd8d6b168ac6a6a0b44ba514a991676e
Ahmed Abdelslam <ahmed.nabolsy6688@gmail.com>
2023-09-13 13:57:51 +0300

    Trip Model

---
+++ app/Models/Trip.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Bus;
+
+class Trip extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['departure_time', 'arrival_time'];
+
+    public function bus()
+    {
+        return $this->belongsTo(Bus::class);
+    }
+}
